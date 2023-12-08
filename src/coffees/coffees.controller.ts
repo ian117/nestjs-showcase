2484529e3f48bb3db31aae8d2e7d1445820140f3
Ian Vassallo <spideramerica71@gmail.com>
2023-12-07 18:49:48 -0600

    feat: examples of REST basics

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,14 +1,44 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(): string {
-    return 'I am returning all strings of coffees';
+  findAll(@Query() query): string {
+    const { limit, offset } = query;
+    return `I am returning all strings of coffees, with pagination: ${limit} <&&> ${offset}`;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `I am returning this coffe: ${id}`;
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.GONE)
+  create(@Body() body) {
+    return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id, @Body() body) {
+    return `${id} fue actualizado con ${body}`;
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  remove(@Param('id') id) {
+    return `${id} fue borrado`;
   }
 }
