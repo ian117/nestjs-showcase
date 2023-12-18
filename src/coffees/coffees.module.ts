c807fecdb26c7eaf59364d6eba8e16596f629c6b
Ian Vassallo <spideramerica71@gmail.com>
2023-12-18 11:24:37 -0600

    feat: coffees service & flavors entity and relation

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
-import { CoffeesController } from './coffees.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Coffee } from './entities/coffee.entity';
+import { Flavor } from './entities/flavor.entity';
+
 import { CoffeesService } from './coffees.service';
+
+import { CoffeesController } from './coffees.controller';
 
 @Module({
     controllers: [CoffeesController],
     exports: [],
-    imports: [],
+    imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
     providers: [CoffeesService],
 })
 export class CoffeesModule {}
