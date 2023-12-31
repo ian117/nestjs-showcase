4388cf82e303744dc45ae8fc748beffe219440b0
Ian Vassallo <spideramerica71@gmail.com>
2023-12-31 10:58:09 -0600

    feat: filters playing around

--- src/common/filters/filter-test-order.filter.ts
+++ src/common/filters/filter-test-order.filter.ts
@@ -4,26 +4,27 @@ import {
     ExceptionFilter,
     HttpException,
 } from '@nestjs/common';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
 @Catch()
 export class TestOrderFilter<T> implements ExceptionFilter {
     catch(exception: HttpException, host: ArgumentsHost) {
-        const ctx = host.switchToHttp();
-        const response = ctx.getResponse<Response>();
-        const request = ctx.getRequest<Request>();
-        const status = exception.getStatus();
+        if (host.getType() === 'http') {
+            const ctx = host.switchToHttp();
+            const response = ctx.getResponse<Response>();
+            const status = exception.getStatus();
+            const exceptionResponse = exception.getResponse();
+            const error =
+                typeof response === 'string'
+                    ? { message: exceptionResponse }
+                    : (exceptionResponse as object);
 
-        console.log('Filter TestOrder');
-        console.log('I will pass here for a second');
-
-        response.status(status).json({
-            statusCode: status,
-            timestamp: new Date().toISOString(),
-            path: request.url,
-            messageOriginal: 'This Is Halo',
-        });
-
-        // throw new Error('Hello Vietnam');
+            response.status(status).json({
+                ...error,
+                timestamp: new Date().toISOString(),
+            });
+        } else if (host.getType() === 'rpc') {
+            // do something that is only important in the context of Microservice requests
+        }
     }
 }
