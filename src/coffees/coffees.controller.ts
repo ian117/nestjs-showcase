53fd4efa2d8afad2b5ebeb9a262e4e799dfee791
Ian Vassallo <spideramerica71@gmail.com>
2024-01-02 17:36:11 -0600

    docs: swagger integration

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -15,11 +15,19 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dyo/pagination-query.dyo';
 import { ApiKeyGuard } from 'src/common/guards/api-key.guard';
 import { Public } from 'src/common/decorators/public.decorator';
+import {
+    ApiForbiddenResponse,
+    ApiOkResponse,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Coffees')
 @Controller('coffees')
 export class CoffeesController {
     constructor(private readonly coffeesService: CoffeesService) {}
 
+    @ApiForbiddenResponse({ status: 403, description: 'Forbidden if not key' })
     @UseGuards(ApiKeyGuard)
     @Public()
     @Get()
@@ -32,6 +40,7 @@ export class CoffeesController {
         return this.coffeesService.findOne(id);
     }
 
+    @ApiResponse({ status: 201, description: 'Coffee Creation' })
     @Post()
     create(@Body() createCoffeeDto: CreateCoffeeDto) {
         return this.coffeesService.create(createCoffeeDto);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -15,6 +16,17 @@ async function bootstrap() {
             },
         }),
     );
+
+    const options = new DocumentBuilder()
+        .setTitle('Iluvcoffee')
+        .setDescription('Coffee application')
+        .setVersion('1.0')
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+
+    SwaggerModule.setup('docs', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
